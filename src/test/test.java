e517540bf6cdc5012a96d2395ccaacf07cb5cb69
WIN_NT\x15g031 <x15g031@chiba-fjb.ac.jp>
2017-04-17 12:36:32 +0900

    枝分かれ

--- src/test/test.java
+++ src/test/test.java
@@ -5,7 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("Gitテスト");
-		System.out.println("追加");
+		System.out.println("枝分かれ");
 
 	}
 
